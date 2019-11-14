70655e453df16e5e9270cb0774eed6eb599eeb67
tremperb <55967770+tremperb@users.noreply.github.com>
2019-11-13 18:13:57 -0800

    test

---
+++ Dropped Objects/Assets/hazardSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hazardSpawner : MonoBehaviour {
+    public GameObject hazard;
+    float randY;
+    Vector2 whereToSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+    // GameObject.GetComponent(Movement).speed = 1;
+    // Use this for initialization
+    void Start () {
+       // GameObject.GetComponent(Movement).moveSpeed = 1;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randY = Random.Range(-4f, 4f);
+            whereToSpawn = new Vector2(transform.position.x, randY);
+            Instantiate(hazard, whereToSpawn, Quaternion.identity);
+        } 
+        
+    }
+
+}
---
+++ Dropped Objects/Assets/randomSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomSpawner : MonoBehaviour {
+    private int rand;
+    public Sprite[] Sprite_Pic;
+    Vector2 temp;
+    private int randSize;
+    public float rotateSpeed = .0f;
+	// Use this for initialization
+	void Start () {
+        getRandSize();
+
+        rand = Random.Range(0, Sprite_Pic.Length);
+        GetComponent<SpriteRenderer>().sprite = Sprite_Pic[rand];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void getRandSize ()
+    {
+        randSize = Random.Range(1, 4);
+        temp = transform.localScale;
+        temp.x = randSize;
+        temp.y = randSize;
+        transform.localScale = temp;
+    }
+}
